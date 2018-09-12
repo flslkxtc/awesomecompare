8b26269664950e7b0558073d949d5f58f219b70b
Stanislav <s.myachenkov@gmail.com>
2018-09-13 01:36:37 +0300

    filesCompare cleanup

--- src/command/fileCompare.ts
+++ src/command/fileCompare.ts
@@ -19,20 +19,11 @@ export abstract class FileCompare {
     }
 
     public async compare() {
-
         let second = await window.showOpenDialog(this.dialogOptions);
         if (second === undefined) {
             console.log('Second file is not selected!');
             return;
         }
-        /*let second = window.showOpenDialog(this.dialogOptions).then(file => {
-                if (!file) {throw Error();} 
-                return file[0];
-            }
-        ).then(err => {
-            console.log('Second file is not selected!');
-         });
-         */
         console.log('Comparing ' + this.first + ' and ' + second[0]);
         commands.executeCommand('vscode.diff', this.first, second[0], 'title');
     }
